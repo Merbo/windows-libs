076d58c23088caa7a2ab46870380967eee7d5fef
Adam <Adam@anope.org>
2011-03-29 22:10:56 -0400

    Added OpenLDAP sources and binaries.

--- install.bat
+++ install.bat
@@ -10,6 +10,10 @@ copy libiconv-1.13.1\bin\* installed\
 copy libiconv-1.13.1\include\* installed\
 copy libiconv-1.13.1\lib\* installed\
 
+copy openldap-2.4.23\bin\* installed\
+copy openldap-2.4.23\include\* installed\
+copy openldap-2.4.23\lib\* installed\
+
 copy mingw\lib\* installed\
 
 mkdir installed\mysql\
